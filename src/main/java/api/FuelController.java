98ca46db8b1b29ac78ab295076587c0522c42727
Kert <juluvanahd@gmail.com>
2019-10-20 17:54:15 +0300

    made new request

--- src/main/java/api/FuelController.java
+++ src/main/java/api/FuelController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.time.Month;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class FuelController {
@@ -34,6 +40,7 @@ public class FuelController {
         double liters = data.getLiters();
         String date = data.getDate();
         double totalPrice = price * liters;
+        totalPrice = Round(totalPrice, 2);
         return fuelRepository.save(new Data(driverID, fuelType, price, liters, date, totalPrice));
     }
 
@@ -78,9 +85,64 @@ public class FuelController {
             }
         }
 
-        ModelAndView result = new ModelAndView("result");
+        ModelAndView result = new ModelAndView("resultFuel");
         model.addAttribute("list", endResult);
 
         return result;
+    }
+
+    @RequestMapping("/resultMoney")
+    public ModelAndView Result(
+            @RequestParam(value = "driverID", required = false, defaultValue = "-1") String driverID,
+            Model model) {
+
+
+        List<Data> data = new ArrayList<>();
+        List<Total> total = new ArrayList<>();
+
+        if(Integer.parseInt(driverID) != -1)
+        {
+            data = fuelRepository.findByDriverID(Integer.parseInt(driverID));
+        }
+        else
+        {
+            data = fuelRepository.findAll();
+        }
+
+        double totalMoneySpent;
+        int usedMonth;
+        for (int j = 1; j <= 12; j++)
+        {
+            usedMonth = 0;
+            totalMoneySpent = 0;
+            for (int i = 0; i < data.size(); i++)
+            {
+                String str[] = data.get(i).getDate().split("-");
+                int dateMonth = Integer.parseInt(str[1]);
+                if(dateMonth == j)
+                {
+                    totalMoneySpent = totalMoneySpent + data.get(i).getTotalPrice();
+                    usedMonth = j;
+                }
+            }
+            if(usedMonth != 0)
+            {
+                totalMoneySpent = Round(totalMoneySpent, 2);
+                total.add(new Total(Month.of(usedMonth).getDisplayName(TextStyle.FULL_STANDALONE, Locale.US), totalMoneySpent));
+            }
+        }
+
+        ModelAndView result = new ModelAndView("resultMoney");
+        model.addAttribute("list", total);
+
+        return result;
+    }
+    
+    public static double Round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
---
+++ src/main/java/api/Total.java
@@ -0,0 +1,25 @@
+package api;
+
+public class Total {
+
+    private String month;
+    private double total;
+
+    public Total(String month, double total)
+    {
+        this.setMonth(month);
+        this.setTotal(total);
+    }
+
+    public void setMonth(String month) { this.month = month; }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setTotal(double total) { this.total = total; }
+
+    public double getTotal() {
+        return total;
+    }
+}
