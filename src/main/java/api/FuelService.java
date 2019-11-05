ba1455f06b9f5582230a0d49a366dcf126f867ef
Kert <juluvanahd@gmail.com>
2019-11-05 18:42:44 +0200

    up

--- src/main/java/api/FuelService.java
+++ src/main/java/api/FuelService.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public interface FuelService {
 
-    public List<String> addDriver(Data data);
-    public List<Data> specifiedMonth(String driverID, String month, FuelRepository fuelRepository);
-    public List<Total> money(String driverID, FuelRepository fuelRepository);
-    public List<Fuel> fuel(String driverID, String fuelType, FuelRepository fuelRepository);
+    List<String> addDriver(Data data);
+    List<Data> specifiedMonth(String driverID, String month, FuelRepository fuelRepository);
+    List<Total> money(String driverID, FuelRepository fuelRepository);
+    List<Fuel> fuel(String driverID, String fuelType, FuelRepository fuelRepository);
 }
